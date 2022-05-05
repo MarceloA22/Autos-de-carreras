bc3b459636fc17790576a5594a075b6e7ddb0987
MarceloA22 <104115950+MarceloA22@users.noreply.github.com>
2022-05-05 17:10:43 -0300

    cambios del let vuelta1 2 3 4 Se cambio la forma de pasar la variable "vuelta" a number=Number

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ rotulo3.innerHTML = "Ingrese la vuelta N°3";
 rotulo4.innerHTML = "Ingrese la vuelta N°4";
 let btnCalcular = document.getElementById("btnCalcular");
 btnCalcular.addEventListener("click", () => {
-  let vuelta1: number = dato1.value * 1;
-  let vuelta2: number = dato2.value * 1;
-  let vuelta3: number = dato3.value * 1;
-  let vuelta4: number = dato4.value * 1;
+  let vuelta1: number = Number(dato1.value);
+  let vuelta2: number = Number(dato2.value);
+  let vuelta3: number = Number(dato3.value);
+  let vuelta4: number = Number(dato4.value);
   let tiempoTotal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
   console.log("Tiempo total:", tiempoTotal);
   console.log("Tiempo promedio", tiempoTotal / 4);
